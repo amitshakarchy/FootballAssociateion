5f438262207233f35f2d0abfd89c9a61e75f87fd
yuvalbe12 <ybene@post.bgu.ac.il>
2020-04-05 20:01:23 +0300

    I started doing the player and coach. I created an abstract page for a personal page and also an interface because I saw that for each stakeholder there are different details that are displayed on the personal page. For example, the player has the date of birth and the coach has a type of training. That's why I created an interface that describes the contract of the two actions together. I wrote most of the code implementation in the abstract department. And the differences between the two I wrote in new departments that are expanding the abstract department. I've added an ENUM of a status that uses the system. Once the user logs in or out of the system, the status changes. I have seen that the status affects actions to be shown to the user in alerts. I have added the types of training the coaches have. I have added group types of roles to coaches. I also added package of tests

--- src/Users/ETraining.java
+++ src/Users/ETraining.java
@@ -5,6 +5,4 @@ public enum ETraining {
     UEFAA,
     UEFAB,
     UEFAPro
-
-
 }
---
+++ tests/Users/CoachTest.java
@@ -0,0 +1,60 @@
+package Users;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CoachTest {
+
+    @Test
+    void addFeedToMyPage() {
+    }
+
+    @Test
+    void addFeedToMyPage1() {
+    }
+
+    @Test
+    void getMyPage() {
+    }
+
+    @Test
+    void getTraining() {
+    }
+
+    @Test
+    void setTraining() {
+    }
+
+    @Test
+    void getRole() {
+    }
+
+    @Test
+    void setRole() {
+    }
+
+    @Test
+    void setUserSettings() {
+    }
+
+    @Test
+    void logout() {
+    }
+
+    @Test
+    void viewProfile() {
+    }
+
+    @Test
+    void getUID() {
+    }
+
+    @Test
+    void getfName() {
+    }
+
+    @Test
+    void getlName() {
+    }
+}
