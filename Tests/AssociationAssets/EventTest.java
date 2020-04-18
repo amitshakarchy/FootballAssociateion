ee6ec6543c2f6fbbada8a476133ef41aeeb0366d
ybene <45098168+ybene1@users.noreply.github.com>
2020-04-18 16:58:40 +0300

    add event test from the previous version of the code, the test was deleted (#13)

---
+++ Tests/AssociationAssets/EventTest.java
@@ -0,0 +1,72 @@
+package AssociationAssets;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.Time;
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+public class EventTest {
+
+    Event e;
+
+    @Before
+    public void setUp() throws Exception {
+
+        Date date= new Date(2020,12,12);
+        Time time= new Time(19,21,0);
+        EEventType eEventType= EEventType.FOUL;
+        e= new Event(date,time,eEventType,"Messi NOOO!!!");
+    }
+
+    @Test
+    public void getDate() {
+        Date date= new Date(2020,12,12);
+        assertEquals(e.date.getDate(),date.getDate());
+    }
+
+    @Test
+    public void setDate() {
+        Date date= new Date(2030,13,13);
+        e.setDate(date);
+        assertEquals(e.date.getDate(),date.getDate());
+    }
+
+    @Test
+    public void getTime() {
+        Time time= new Time(19,21,0);
+        assertEquals(e.time.getTime(),time.getTime());
+    }
+
+    @Test
+    public void setTime() {
+        Time time= new Time(18,28,0);
+        e.setTime(time);
+        assertEquals(e.time.getTime(),time.getTime());
+    }
+
+    @Test
+    public void getEventType() {
+        assertEquals(e.getEventType(),EEventType.FOUL);
+    }
+
+    @Test
+    public void setEventType() {
+        e.setEventType(EEventType.GOAL);
+        assertEquals(e.getEventType(),EEventType.GOAL);
+    }
+
+    @Test
+    public void getDescription() {
+        assertEquals(e.getDescription(),"Messi NOOO!!!");
+    }
+
+    @Test
+    public void setDescription() {
+        e.setDescription("Messi YESSS!!!");
+        assertEquals(e.getDescription(),"Messi YESSS!!!");
+
+    }
+}
