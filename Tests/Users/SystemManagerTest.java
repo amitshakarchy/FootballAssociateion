2ae4f6aa0377381a2c50910ea9675262c0146fcc
yuvalbe12 <ybene@post.bgu.ac.il>
2020-04-18 20:22:01 +0300

    add removing users + getter in season

--- Tests/Users/SystemManagerTest.java
+++ Tests/Users/SystemManagerTest.java
@@ -1,24 +1,63 @@
 package Users;
 import AssociationAssets.*;
 import javafx.util.Pair;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import System.*;
 
 import java.io.File;
+import java.sql.Time;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SystemManagerTest {
+    Team team1, team2;
+    Player p;
+    Coach c;
+    Field field;
+    TeamManager m;
+    TeamOwner o;
+    Season season;
+    Referee r1,r2,r3;
+    Game game;
+    @Before
+    public void setUp() throws Exception {
+        c= new Coach("1","l","a", null, null);
+        p= new Player("1", "a", "a", null, null);
+        m= new TeamManager("1", "a", "a");
+        o= new TeamOwner("1", "a", "a");
+        field= new Field("Teddy", "Jerusalem", 31000);
+        season=new Season("season");
 
+        r1= new Referee("1","a","a",EReferee.MAIN);
+        r2= new Referee("2","a","a",EReferee.VAR);
+        r3= new Referee("3","a","a",EReferee.VAR);
+        team1= new Team(1,"Barcelona", season, null,null,null);
+        team2= new Team(2, "Beitar",season, null,null,null);
+        team2.addField(field);
+        game= new Game(new Date(2021,10,2020),
+                new Time(19,30,0),
+                field,team1,team2,r1,r2,r3,season,
+                new League("l"));
+
+    }
     @Test
     void closeTeam() {
+        try {
+            setUp();
+            team1.addHomeGame(game);
+            FootballSystem.getInstance().addTeamToDB(team1);
+            SystemManager systemManager = new SystemManager("new","new","new");
+            systemManager.closeTeam(String.valueOf(team1.getName()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    @Test
-    void removeUser() {
-    }
 
     @Test
     void getComplainsAndresponseOnComplain() {
--- src/Users/SystemManager.java
+++ src/Users/SystemManager.java
@@ -1,12 +1,9 @@
 package Users;
-import AssociationAssets.EGameStatus;
-import AssociationAssets.ETeamStatus;
-import AssociationAssets.Game;
-import AssociationAssets.Team;
+import AssociationAssets.*;
 import RecommendationSystem.*;
 import javafx.util.Pair;
-
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import System.*;
@@ -53,8 +50,141 @@ public class SystemManager extends Fan {
         }
     }
 
-    public void removeUser(){} //useCase 8.2
+    /**
+     * This function deletes a coach, before deleting it makes sure that the
+     * coach is not used as a coach in the future season
+     *
+     * @param userName - coach's userName to delete
+     *
+     * # use case 8.2
+     */
+    public void removeCoach(String userName){
+        Coach coach = FootballSystem.getInstance().getCoachByUserName(userName);
+        if(coach != null){
+            //Checks whether he coaches a particular team in a future season
+            for (Season season:
+                    FootballSystem.getInstance().getSeasonDB().getAllSeasons().values()) {
+              if(Integer.parseInt(season.getYear()) > Calendar.getInstance().get(Calendar.YEAR)){
+                  for (AdditionalInfo additional:
+                       season.getTeamAdditionalInfo().values()) {
+                      for (String coachToCompare:
+                           additional.getCoaches()) {
+                          if(coachToCompare.equals(userName)){
+                              return;
+                          }
+                      }
+                  }
+                }
+            }
+            FootballSystem.getInstance().removeUser(userName);
+            Logger.getInstance().addActionToLogger("Coach: "+userName+" deleted by the system manager userName: "+getUserName());
+        }
+    }
 
+    /**
+     * This function deletes a player, before deleting it makes sure the
+     * player is not used as a player in a future season
+     *
+     * @param userName - player's userName to delete
+     *
+     *  # use case 8.2
+     */
+    public void removePlayer(String userName){
+        Player player = FootballSystem.getInstance().getPlayerByUserName(userName);
+        if(player != null){
+            //Checks whether he coaches a particular team in a future season
+            for (Season season:
+                    FootballSystem.getInstance().getSeasonDB().getAllSeasons().values()) {
+                if(Integer.parseInt(season.getYear()) > Calendar.getInstance().get(Calendar.YEAR)){
+                    for (AdditionalInfo additionalInfo:
+                            season.getTeamAdditionalInfo().values()) {
+                        for (String playerToCompare:
+                                additionalInfo.getPlayers()) {
+                            if(playerToCompare.equals(userName)){
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+            FootballSystem.getInstance().removeUser(userName);
+            Logger.getInstance().addActionToLogger("Player: "+userName+" deleted by the system manager userName: "+getUserName());
+        }
+    }
+
+    /**
+     * This function deletes a team owner - if not used as a team owner
+     *  in the future
+     *
+     * @param userName- Team Owner's userName to delete
+     *
+     *# use case 8.2
+     */
+    public void removeTeamOwner(String userName){
+        TeamOwner teamOwner = FootballSystem.getInstance().getTeamOwnerByUserName(userName);
+        if(teamOwner != null){
+            //Checks whether he coaches a particular team in a future season
+            for (Season season:
+                    FootballSystem.getInstance().getSeasonDB().getAllSeasons().values()) {
+                if(Integer.parseInt(season.getYear()) > Calendar.getInstance().get(Calendar.YEAR)){
+                    for (AdditionalInfo additionalInfo:
+                            season.getTeamAdditionalInfo().values()) {
+                        for (String teamOwnerToCompare:
+                                additionalInfo.getTeamOwnersHashSet()) {
+                            if(teamOwnerToCompare.equals(userName)){
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+            FootballSystem.getInstance().removeUser(userName);
+            Logger.getInstance().addActionToLogger("Team Owner: "+userName+" deleted by the system manager userName: "+getUserName());
+        }
+    }
+
+    /**
+     * This function deletes an administrator from the system if at least one exists.
+     *
+     * @param userName - System Manager's userName to delete
+     *
+     *  # use case 8.2
+     */
+    public void removeSystemManager(String userName){
+        SystemManager systemManager = FootballSystem.getInstance().getSystemManagerByUserName(userName);
+        if(systemManager != null){
+            if(FootballSystem.getInstance().getSystemManagerMap().values().size() == 1){
+                return;
+            }
+            FootballSystem.getInstance().removeUser(userName);
+            Logger.getInstance().addActionToLogger("System Manager: "+userName+" deleted by the system manager userName: "+getUserName());
+        }
+    }
+
+    /**
+     *This function deletes a fan, before deleting it verifies that the fan
+     *  is not either: Group owner or player or coach or referee or
+     *  administrator or association representative or team administrator
+     *
+     * @param userName - fan's userName to delete
+     *
+     *  # use case 8.2
+     */
+    public void removeFan(String userName){
+        Fan fan = FootballSystem.getInstance().getFanByUserName(userName);
+        if(FootballSystem.getInstance().getRefereeByUseName(userName) != null ||
+                FootballSystem.getInstance().getSystemManagerByUserName(userName) != null ||
+                FootballSystem.getInstance().getRepresentativeFootballAssociationByUseName(userName) != null||
+                FootballSystem.getInstance().getPlayerByUserName(userName) != null||
+                FootballSystem.getInstance().getCoachByUserName(userName) != null||
+                FootballSystem.getInstance().getTeamOwnerByUserName(userName) != null||
+                FootballSystem.getInstance().getTeamManagerByUserName(userName) != null) {
+                return;
+        }
+            FootballSystem.getInstance().removeUser(userName);
+            Logger.getInstance().addActionToLogger("Fan: "+userName+" deleted by the system manager userName: "+getUserName());
+
+    }
 
     /**
      *  An administrator can view the complaint box
