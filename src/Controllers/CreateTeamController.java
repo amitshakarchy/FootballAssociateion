ef4b4f952262c1d9a423ddff0e129d860b4cbde6
Tair Cohen <45098383+taircohen94@users.noreply.github.com>
2020-05-05 20:17:15 +0300

    Javafx (#26)
    * Added the first controller anf fxml files. added main
    
    * first steps for GUI
    
    * Only GUI - login , create new team, main page, manage team!
    
    * merge with new master,
    also - manage game and manage policy was add with controllers
    
    * merge with new master,
    also - manage game and manage policy was add with controllers
    
    Co-authored-by: amitshakarchy <58327011+amitshakarchy@users.noreply.github.com>

--- src/Controllers/CreateTeamController.java
+++ src/Controllers/CreateTeamController.java
@@ -6,12 +6,29 @@ import javafx.scene.control.Alert;
 
 public class CreateTeamController {
 
+
+    public RequiredField requiredField1;
+    public RequiredField requiredField2;
+    public RequiredField requiredField3;
+    public RequiredField requiredField4;
+    public RequiredField requiredField5;
+    public RequiredField requiredField6;
+
     @FXML
     public void clickOnCreateTeam(ActionEvent e) {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Information Dialog");
-        alert.setContentText("The request has been sent to the \n" +
-                " Representative Football Association!");
-        alert.showAndWait();
+        requiredField1.eval();
+        requiredField2.eval();
+        requiredField3.eval();
+        requiredField4.eval();
+        requiredField5.eval();
+        requiredField6.eval();
+        if(!requiredField1.getHasErrors() && !requiredField2.getHasErrors() && !requiredField3.getHasErrors() &&
+                !requiredField4.getHasErrors() && !requiredField5.getHasErrors()  && !requiredField6.getHasErrors()  ){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Information Dialog");
+            alert.setContentText("The request has been sent to the \n" +
+                    " Representative Football Association!");
+            alert.showAndWait();
+        }
     }
 }
--- src/Controllers/MainPageController.java
+++ src/Controllers/MainPageController.java
@@ -1,4 +1,49 @@
 package Controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import java.io.IOException;
+
 public class MainPageController {
+
+    public Button manageTeamBtn;
+    public Button managePolicyBtn;
+    public Button manageGameBtn;
+
+    @FXML
+    public void manageTeamClick() {
+        // TODO: 5/3/2020 need to validate the permission of the user
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../View/ManageTeam.fxml"));
+        setCorrectWindow(loader, manageTeamBtn,"Manage Team");
+    }
+
+    @FXML
+    public void managePolicyClick() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../View/ManagePolicy.fxml"));
+        setCorrectWindow(loader, managePolicyBtn,"Manage Policy");
+    }
+
+    @FXML
+    public void manageGameClick() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../View/ManageGame.fxml"));
+        setCorrectWindow(loader,manageGameBtn,"Manage Game");
+    }
+
+    private void setCorrectWindow(FXMLLoader loader, Button managePolicyBtn,String title) {
+        Stage stage = new Stage();
+        stage.initOwner(managePolicyBtn.getScene().getWindow());
+        try {
+            stage.setScene(new Scene((Parent) loader.load()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        stage.setTitle(title);
+        // showAndWait will block execution until the window closes...
+        stage.showAndWait();
+    }
+
 }
