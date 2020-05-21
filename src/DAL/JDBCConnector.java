d4e94f45af412d977a013424990a05ee5cb501b0
amitshakarchy <58327011+amitshakarchy@users.noreply.github.com>
2020-05-21 17:40:56 +0300

    Connect db (#44)
    * Added a connection with the DB.
    Added methods for uploading teams, leagues, seasons etc. to the system
    
    * added methods for uploading
    
    * added methods for uploading
    
    * added game uploader
    
    * Finished seasonLeagueBinder
    
    * Finished data uploading
    
    * Finished data uploading

---
+++ src/DAL/JDBCConnector.java
@@ -0,0 +1,35 @@
+package DAL;
+
+import AssociationAssets.*;
+import System.*;
+import Users.*;
+
+import java.util.*;
+
+/**
+ * JDBC Connector in in charge of connecting with the DB.
+ * Offers uploading and downloading all data do/from the system's datasets.
+ */
+public class JDBCConnector {
+
+    DatabaseManager databaseManager;
+
+    public void connectDBUploadData() {
+
+        databaseManager = new DatabaseManagerServerMySQL();
+        databaseManager.startConnection();
+
+        DataUploader uploader = new DataUploader(databaseManager);
+        uploader.uploadData();
+
+        databaseManager.closeConnection();
+    }
+
+    public void connectSaveOnDB() {
+
+    }
+
+
+
+
+}
