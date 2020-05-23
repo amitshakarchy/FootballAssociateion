93bc80f0cff27d9edc2fd8f5f4d8f77e281b73d4
amitshakarchy <58327011+amitshakarchy@users.noreply.github.com>
2020-05-23 14:35:09 +0300

    Added push methods

--- Tests/DAL/DataUploaderTest.java
+++ Tests/DAL/DataUploaderTest.java
@@ -1,9 +1,6 @@
 package DAL;
 
 import AssociationAssets.League;
-import Users.Fan;
-import org.junit.Before;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
