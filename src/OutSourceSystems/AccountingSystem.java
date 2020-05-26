a17f8bd36b2e5c119fbd1a6aacbdd6e4ffc86110
yarinHayun <62398145+yarinHayun@users.noreply.github.com>
2020-05-26 23:22:02 +0300

    connect to out source systems

--- src/OutSourceSystems/AccountingSystem.java
+++ src/OutSourceSystems/AccountingSystem.java
@@ -1,7 +1,14 @@
 package OutSourceSystems;
 
-public class AccountingSystem {
-    public static void connect(){
+public class AccountingSystem implements IAccountingSystem {
 
+    @Override
+    public boolean connect() {
+        return true;
+    }
+
+    @Override
+    public boolean addPayment(String teamName, String date, double amount) {
+        return true;
     }
 }
